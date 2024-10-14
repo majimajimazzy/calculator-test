bb0baf12916145e23a7d8d1b57f06c1a2dce24dd
majimajimazzy <j320252@g.kogakuin.jp>
2024-10-14 11:02:31 +0900

    完成

---
+++ work/js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",function(){
+  const display = document.getElementById("display");
+  const someNumbers = document.querySelectorAll(".number");
+  const someOperators = document.querySelectorAll(".operator");
+  const equal=document.querySelector('.equals');
+  const clear=document.querySelector('.clear');
+  for(let i=0;i<someNumbers.length;i++){
+    someNumbers[i].addEventListener('click',function(){
+      display.value+=this.getAttribute('data-number');
+    });
+  }
+  for(let i=0;i<someOperators.length;i++){
+    someOperators[i].addEventListener('click',function(){
+      display.value+=' '+this.getAttribute('data-operator')+' ';
+    });
+  }
+  equal.addEventListener('click',function(){
+    display.value=eval(display.value);
+  });
+  clear.addEventListener('click',function(){
+    display.value='';
+  });});
